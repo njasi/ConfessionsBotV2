7e818077d6cf03b7b73ed571bee26d20ba7c99de
Nick <nick@jasinski3.com>
2021-04-18 13:49:34 -0400

    h

--- server/db/db.js
+++ server/db/db.js
@@ -14,7 +14,7 @@ const db = new Sequelize(
     //   // https://github.com/sequelize/sequelize/issues/12083#issuecomment-648870469
     //   require:true,
     //   // https://stackoverflow.com/questions/58965011/sequelizeconnectionerror-self-signed-certificate
-    //   rejectUnauthorized: false,
+      rejectUnauthorized: false,
     },
   }
 );
