d39d3ac1716c1f522d23d79c079ca5042444169d
Nick <nick@jasinski3.com>
2021-04-18 15:18:35 -0400

    npm auto update

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,13 @@ const databaseName = "confessions_bot";
 
 console.log("setup sequelize");
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false,
+let options = {
+  logging: false,
+};
+
+if (process.env.NODE_ENV == "production") {
+  options = {
+    ...options,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -15,35 +18,12 @@ const db = new Sequelize(
         rejectUnauthorized: false,
       },
     },
-    // https://stackoverflow.com/questions/25000183/node-js-postgresql-error-no-pg-hba-conf-entry-for-host
-    // dialect: "postgres",
+  };
+}
 
-    // dialectOptions: {
-    //   ssl: true,
-    //   //   // https://github.com/sequelize/sequelize/issues/12083#issuecomment-648870469
-    //   require: true,
-    //   //   // https://stackoverflow.com/questions/58965011/sequelizeconnectionerror-self-signed-certificate
-    //   rejectUnauthorized: false,
-    // },
-  }
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  options
 );
-
-// const db = new Sequelize({
-//   development: {
-//     database: `postgres://localhost:5432/${databaseName}`,
-//     host: "127.0.0.1",
-//     dialect: "postgres",
-//   },
-//   production: {
-//     use_env_variable: "DATABASE_URL",
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   },
-// });
 
 module.exports = db;
