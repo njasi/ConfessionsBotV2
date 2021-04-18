0a38bc29bc8da6dd83f997dbf731fdd36610ee11
Nick <nick@jasinski3.com>
2021-04-18 14:31:47 -0400

    npm auto update

---
+++ script/set_env_heroku.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import re
+
+ENV_MODES = {
+    "dev": ".env_test",
+    "dep": ".env_deploy"
+}
+
+
+
+def main(mode):
+    file = open(ENV_MODES[mode])
+    lines = filter(lambda line: re.search("^[^#.*]",line) and not line.isspace(),file.readlines())
+    command = "heroku config:set"
+    for line in lines:
+      command += " {}".format(line.strip())
+    # print(command)
+    os.system(command)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) < 2 or sys.argv[1] not in ENV_MODES):
+        print("Usage: set_env_heroku.py mode\n\tmode: {}".format("|".join(ENV_MODES)))
+    else:
+        main(sys.argv[1])
