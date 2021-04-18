96de1700b2c63a7c85f5766ead8c6d4ca77baed7
Nick <nick@jasinski3.com>
2021-04-18 13:24:31 -0400

    oog

--- server/db/db.js
+++ server/db/db.js
@@ -6,10 +6,13 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
+    // https://stackoverflow.com/questions/25000183/node-js-postgresql-error-no-pg-hba-conf-entry-for-host
     dialect: "postgres",
     dialectOptions: {
       ssl: true,
     },
+    // https://stackoverflow.com/questions/58965011/sequelizeconnectionerror-self-signed-certificate
+    rejectUnauthorized: false,
   }
 );
 
