c7ab0f9b479285f332fbca902d9c0bffc30cccf7
Nick <nick@jasinski3.com>
2023-01-19 01:09:42 -0800

    Update db.js
    use username and password from env

--- server/db/db.js
+++ server/db/db.js
@@ -1,29 +1,12 @@
 const Sequelize = require("sequelize");
 
-const databaseName = "confessions_bot";
-
-let options = {
-  logging: (query) => {
-    // console.log("\n", query, "\n");
-  },
-};
-
-if (process.env.NODE_ENV == "production") {
-  options = {
-    ...options,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  };
-}
-
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  options
+  process.env.DATABASE_NAME,
+  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_PASSWORD,
+  {
+    dialect: "postgres",
+    logging: false,
+  }
 );
-
 module.exports = db;
